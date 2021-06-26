487e2f55b204ba70f6cf94a98cfdb7f27396f9ee
ManoJarvis <manothetiger27@gmail.com>
2021-06-26 16:03:37 +0530

    title modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@
 function Home(){
     return(
         <div>
-            <h1 className="h1 text-center mt-5 text-secondary">This is the Home page of this application</h1>
+            <h1 className="h1 text-center mt-5 text-secondary">This is the Home page of this app</h1>
         </div>
     )
 }
